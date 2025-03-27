5e21514556bbb2cc43f36b2c7d3b4af865bc1e5c
JDCodes2004 <145573608+JDCodes2004@users.noreply.github.com>
2025-03-27 15:36:46 +0000

    Room List work start

---
+++ Assets/RoomList.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class RoomList : MonoBehaviourPunCallbacks
+{
+    [Header("UI")] public Transform roomListParent;
+    public GameObject roomListItemPrefab;
+
+
+    private List<RoomInfo> cachedRoomList = new List<RoomInfo>();
+
+    IEnumerator Start()
+    {
+        //Precautions
+        if (PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.LeaveRoom();
+            PhotonNetwork.Disconnect();
+        }
+
+        yield return new WaitUntil(() => !PhotonNetwork.IsConnected);
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        if (cachedRoomList.Count <= 0)
+        {
+            cachedRoomList = roomList;
+        }
+
+        else
+        {
+            foreach (var room in roomList)
+            {
+                for (int i = 0; i < cachedRoomList.Count; i++)
+                {
+                    if (cachedRoomList[i].Name == room.Name)
+                    {
+                        List<RoomInfo> newList = cachedRoomList;
+
+
+                        if (room.RemovedFromList)
+                        {
+                            newList.Remove(newList[i]);
+                        }
+                        else
+                        {
+                            newList[i] = room;
+                        }
+                    }
+                }
+            }
+        }
+
+        UpdateUI();
+    }
+
+
+
+
+    private void UpdateUI()
+    {
+        foreach (Transform roomItem in roomListParent)
+        {
+            Destroy(roomItem.gameObject);
+        }
+
+
+        foreach (var room in cachedRoomList)
+        {
+            GameObject roomItem = Instantiate(roomListItemPrefab, roomListParent);
+
+            roomItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = room.Name;
+            roomItem.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = room.PlayerCount + "/16";
+        }
+    }
+
+
+
+}
+
