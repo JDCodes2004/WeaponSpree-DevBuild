7ec2a27e651941d4c5083fc13888bb31fe6eed27
JDCodes2004 <145573608+JDCodes2004@users.noreply.github.com>
2025-04-28 12:10:56 +0100

    Health UI Update

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public bool isLocalPlayer;
 
     [Header("UI")]
     public TextMeshProUGUI healthText;
+    public GameObject LowHealthWarning;
+    public AudioSource LowHealth;
+    public AudioSource CriticalHealth;
 
     [PunRPC]
     public void TakeDamage (int _damage)
@@ -18,16 +22,36 @@ public bool isLocalPlayer;
         health -= _damage;
 
         healthText.text = health.ToString();
+
+        //Low Health Warning
+
+        if (health <= 50)
+        {
+            LowHealthWarning.SetActive(true);
+            LowHealth.Play();
+        }
+
+        if (health <= 25)
+        {
+            LowHealthWarning.SetActive (true);
+            CriticalHealth.Play();
+        }
+        else
+        {
+            LowHealthWarning.SetActive (false);
+        }
+
+        //Player Death
         if (health <= 0)
         {
             Destroy(gameObject);
-            if (isLocalPlayer) 
+            if (isLocalPlayer)
             {
                 RoomManager.instance.SpawnPlayer();
 
             }
 
-            
+
         }
     }
 }
